e68d410bd1c0a767fbeb730f3f62eeb02690052a
Geovane Borba <geovanne@live.com>
2023-07-07 18:57:13 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,29 @@
-import Image from 'next/image'
+import { ReactElement } from 'react'
+import { NextPageWithLayout } from './_app'
+import { DefaultLayout } from '@/layouts/DefaultLayout'
+import { ChartLineUp } from 'phosphor-react'
+import { PopularBooks } from '@/components/PopularBooks'
+import { HomePageContainer } from '@/components/HomePage/styles'
+import { LatestBookReviews } from '@/components/LastestBookReview'
 
-import backgroundImg from '../assets/background.png'
-
-export default function Home() {
+const HomePage: NextPageWithLayout = () => {
   return (
-    <div className="grid grid-cols-2 p-5 bg-gray-800">
-      <Image
-        src={backgroundImg}
-        alt=""
-        width={598}
-        height={912}
-        priority
-        quality={100}
-      />
-      <div>
-        <div>
-          <h2>Boas Vindas!</h2>
-          <h3>Faça seu login ou acesse como visitante.</h3>
+    <HomePageContainer>
+      <h1>
+        <ChartLineUp size="32" />
+        Início
+      </h1>
 
-          <button>Entrar com Google</button>
-          <button>Entrar com Google</button>
-          <button>Entrar com Google</button>
-        </div>
+      <div>
+        <LatestBookReviews />
+        <PopularBooks />
       </div>
-    </div>
+    </HomePageContainer>
   )
 }
+
+HomePage.getLayout = (page: ReactElement) => {
+  return <DefaultLayout title="Início">{page}</DefaultLayout>
+}
+
+export default HomePage
